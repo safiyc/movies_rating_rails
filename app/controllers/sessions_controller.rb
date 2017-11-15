6fe1066a22d01a18280711f6722a594771650cb9
Luciano Oliveira and Safiy Cham <luciano@>
2017-11-15 13:20:40 -0800

    add navbar with log in status and deadlinks.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,22 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(username: login_params[:username])
+
+    if user && user.authenticate(login_params[:password])
+      login(user)
+      redirect_back fallback_location: new_session_path, notice: "Logged in"
+    else
+      flash.now[:notice] = 'Invalid username / password combination'
+      render :new
+    end
+  end
+
+  private
+
+  def login_params
+    params.require(:session).permit(:username, :password)
+  end
 end
