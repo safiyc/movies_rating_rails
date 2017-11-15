8fe43ff6a36d1a66bfe88a78dba62cb3d3540227
Luciano Oliveira and Safiy Cham <safiy@>
2017-11-15 11:24:56 -0800

    create basic authentication for users.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :users, only: [:new, :create]
 
+  resources :sessions, only: [:new, :create]
+  resources :users, only: [:new, :create]
   resources :movies
   root 'movies#index'
 end
